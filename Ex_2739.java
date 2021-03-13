8e2a779adadd72e145857edc552c079eea6ff299
do0413 <wawa4989@naver.com>
2021-03-13 16:57:54 +0900

    21-03-13 sutdy

--- Ex_2739.java
+++ Ex_2739.java
@@ -1,9 +1,16 @@
 package BeakJoon_;
 
+import java.util.Scanner;
+
 public class Ex_2739 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n= sc.nextInt();
+				
+		for (int i=0; i<9; i++) {
+			System.out.println(n+"*"+(i+1)+"="+n*(i+1));
+		}
 
 	}
 
