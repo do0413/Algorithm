759a036fe020436801701655d78cda5c6c567bc2
do0413 <wawa4989@naver.com>
2021-03-15 00:12:48 +0900

    2021-03-14 study

---
+++ Ex_2741.java
@@ -0,0 +1,24 @@
+package BeakJoon_;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Ex_2741 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for (int i=0; i<n; i++) {
+			bw.write(i+1+"\n");
+		}
+		bw.close();
+		br.close();
+	}
+
+}
---
+++ Ex_2742.java
@@ -0,0 +1,24 @@
+package BeakJoon_;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Ex_2742 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n= Integer.parseInt(br.readLine());
+		
+		for(int i=n; i>0; i--) {
+			bw.write(i+"\n");
+		}
+		bw.close();
+		br.close();
+	}
+
+}
