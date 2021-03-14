2d31dacdbd9e6cf608ebb07ff2c35fc2242cea36
do0413 <wawa4989@naver.com>
2021-03-14 18:40:42 +0900

    2021-03-14 study

--- Ex_8393.java
+++ Ex_8393.java
@@ -1,10 +1,24 @@
 package BeakJoon_;
 
+import java.io.*;
+
 public class Ex_8393 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int num = 0;
+		for(int i=1; i<n+1; i++) {
+			num += i;
+		}
+		bw.write(String.valueOf(num));
+		
+		br.close();
+		bw.close();
+		
 	}
 
 }
